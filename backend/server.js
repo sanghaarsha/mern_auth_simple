c5e17a83ec47b8d7a413bb81c1182ec23f28d9a0
sanghaarsha <sanghaarsha@gmail.com>
2024-12-19 16:04:22 +0545

    working version

--- backend/server.js
+++ backend/server.js
@@ -12,6 +12,7 @@ app.use(express.json());
 app.use(
     cors({
         origin: "http://localhost:5173",
+        methods: ["GET", "POST", "PUT", "DELETE"],
         credentials: true,
     })
 );
@@ -29,7 +30,7 @@ app.use(
 );
 
 // MongoDB Connection
-mongoose.connect("mongodb://localhost:27017/auth-system");
+mongoose.connect("mongodb://127.0.0.1:27017/auth-system");
 
 // User Schema
 const userSchema = new mongoose.Schema({
